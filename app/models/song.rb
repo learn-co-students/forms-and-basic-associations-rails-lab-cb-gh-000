cad0730c5ae5d974588fc98f015b2db0017317e6
EC2 Default User <ec2-user@ip-172-31-40-57.us-east-2.compute.internal>
2018-01-05 10:31:07 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,24 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
+  validates :title, presence: true
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  def artist_name
+    self.artist.name if artist
+  end
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+  def note_contents=(n_contents)
+    n_contents.collect {|content| self.notes.find_or_initialize_by(content: content) unless content.empty?}
+  end
 
+  def note_contents
+    self.notes.collect {|n| n.content}
+  end
+end
